5a356df0f867e7bb5ce49e61b9729f86514bfbcb
Ayman Omer <aymangebrilomer@gmail.com>
2020-11-18 15:37:48 -0500

    homework

--- ducky.js
+++ ducky.js
@@ -1,12 +1,14 @@
 window.onload = function() {
-  const body = document.body;
-
-  console.log(body);
+  
 
   // 1. Create a <div> with the class "duck" and add it to the body.  Do this step by step
+
+
   // ( 1. create the element
   //   2. add a class to the element
   //   3. append the element to the body )
+
+  
 
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
   // https://www.w3schools.com/jsref/met_win_setinterval.asp
@@ -56,4 +58,56 @@ window.onload = function() {
   //     direction the duck is flying and change the way the duck is facing
 
   // Done, you have accomplish another level of skill
+
+  const duck = document.createElement('div');
+    duck.className = 'duck';
+    duck.style.top = Math.random() * window.innerWidth + 'px';
+    duck.style.left = Math.random() * window.innerHeight + 'px';
+    document.body.appendChild(duck);
+    setTimeout(function() { duck.remove() }, 1)
+
+    function createDuck() {
+        const duck = document.createElement('div');
+        duck.className = 'duck';
+        duck.style.top = Math.random() * window.innerWidth + 'px';
+        duck.style.left = Math.random() * window.innerHeight + 'px';
+        document.body.appendChild(duck);
+        let flapper = setInterval(function flapDuck() {
+            duck.classList.toggle('flap');
+        }, 250);
+
+        function moveDuck() {
+            let top = Math.random() * window.innerWidth;
+            let left = Math.random() * window.innerHeight;
+
+            duck.style.top = top + 'px';
+            duck.style.left = left + 'px';
+
+            function addShot() {
+                duck.classList.add('shot');
+                setTimeout(function() { duck.remove(); checkForWinner() }, 1000)
+            }
+
+            duck.addEventListener('click', addShot);
+        }
+
+        moveDuck()
+        let mover = setInterval(moveDuck, 1000);
+        return duck;
+    };
+
+    for (let i = 0; i < 5; i++) {
+        createDuck()
+    }
+
+    function checkForWinner() {
+    if (document.getElementsByClassName('duck').length == 0) {
+        alert('You win!')
+      }
+    }
+
+  
+  
+
+  
 };
