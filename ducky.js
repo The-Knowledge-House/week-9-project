9a5e992ef1e549386c7ef4940abd9971af232959
Jenny <jennyd.moncion@gmail.com>
2021-11-04 00:40:59 -0400

    Duck Homework

--- ducky.js
+++ ducky.js
@@ -1,8 +1,3 @@
-window.onload = function() {
-  const body = document.body;
-
-  console.log(body);
-
   // 1. Create a <div> with the class "duck" and add it to the body.  Do this step by step
   // ( 1. create the element
   //   2. add a class to the element
@@ -56,4 +51,57 @@ window.onload = function() {
   //     direction the duck is flying and change the way the duck is facing
 
   // Done, you have accomplish another level of skill
+
+window.onload = function () {
+  const body = document.body;
+
+  console.log(body);
+
+  const duck = document.createElement('div');
+  duck.className = 'duck';
+  duck.style.top = Math.random() * window.innerWidth + 'px'; 
+  duck.style.left = Math.random() * window.innerHeight + 'px'; 
+  document.body.appendChild(duck);
+  setTimeout(function () { duck.remove() }, 1)
+
+  function createDuck() {
+    const duck = document.createElement('div');
+    duck.className = 'duck';
+    duck.style.top = Math.random() * window.innerWidth + 'px';
+    duck.style.left = Math.random() * window.innerHeight + 'px';
+    document.body.appendChild(duck);
+    let flapper = setInterval(function flapDuck() {
+      duck.classList.toggle('flap');
+    }, 250);
+
+    function moveDuck() {
+      let top = Math.random() * window.innerWidth;
+      let left = Math.random() * window.innerHeight;
+
+      duck.style.top = top + 'px';
+      duck.style.left = left + 'px';
+
+      function addShot() {
+        duck.classList.add('shot');
+        setTimeout(function () { duck.remove(); checkForWinner() }, 1000)
+      }
+
+      duck.addEventListener('click', addShot);
+    }
+
+    moveDuck()
+    let mover = setInterval(moveDuck, 1000);
+    return duck;
+  };
+
+  for (let i = 0; i < 5; i++) {
+    createDuck()
+  }
+
+  function checkForWinner() {
+    if (document.getElementsByClassName('duck').length == 0) {
+      alert('You win!')
+    }
+  }
+    
 };
